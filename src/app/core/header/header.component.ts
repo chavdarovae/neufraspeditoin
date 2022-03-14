578abafa8af4c09a4a817af859afd9b69cc04ae3
Emiliya Chavdarova <emiliya@carrypicker.com>
2022-03-14 16:50:51 +0100

    Adds page title

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -8,6 +8,7 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class HeaderComponent implements OnInit {
   activeTab = 'unternehmen';
+  pageTitle = 'aboutUs';
   logoPath = isDevMode() ? '../../../assets/img/Logo Neufra.jpg' : './assets/img/Logo Neufra.jpg';
   imgPath = isDevMode() ? '../../../assets/img/poster-unternehmen.jpg' : './assets/img/poster-unternehmen.jpg';
 
@@ -18,6 +19,18 @@ export class HeaderComponent implements OnInit {
 
   onNavClick(selectedNavTab: string) {
     this.activeTab = selectedNavTab;
+    switch (selectedNavTab) {
+      case 'unternehmen': this.pageTitle = 'aboutUs'
+      break;
+      case 'standorte': this.pageTitle = 'locations'
+      break;
+      case 'dienstleistungen': this.pageTitle = 'services'
+      break;
+      case 'karriere': this.pageTitle = 'career'
+      break;
+      case 'transportunternehmer': this.pageTitle = 'transportCompany'
+      break;
+    }
     if (isDevMode()) {
       this.imgPath = `../../../assets/img/poster-${selectedNavTab}.jpg`
     } else {
