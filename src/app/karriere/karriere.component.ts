b55d768d671ae5b80f3571204636918e6d881b23
Emiliya Chavdarova <emiliya@carrypicker.com>
2022-03-14 12:00:18 +0100

    Fixes the positions list

--- src/app/karriere/karriere.component.ts
+++ src/app/karriere/karriere.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { openings } from 'src/assets/files/carriers';
 
 @Component({
   selector: 'app-karriere',
@@ -8,14 +9,15 @@ import { Component, OnInit } from '@angular/core';
 export class KarriereComponent implements OnInit {
   currPosition= 0;
   expanded = false;
+  positions = openings;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  togglePosition() {
-    this.currPosition = 1;
+  togglePosition(index: number) {
+    this.currPosition = index;
     this.expanded = !this.expanded;
   }
 
