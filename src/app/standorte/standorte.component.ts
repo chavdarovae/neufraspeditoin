f581bbfc3c296050b0f0c66acefd3e85d242f416
Emiliya Chavdarova <emiliya@carrypicker.com>
2022-03-23 15:37:39 +0100

    Adds new map solution

--- src/app/standorte/standorte.component.ts
+++ src/app/standorte/standorte.component.ts
@@ -1,6 +1,5 @@
 import { Component, isDevMode, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { locationList } from 'src/assets/files/loactions';
 import { StandortDetailsComponent } from './standort-details/standort-details.component';
 
 @Component({
@@ -9,200 +8,14 @@ import { StandortDetailsComponent } from './standort-details/standort-details.co
 	styleUrls: ['./standorte.component.scss']
 })
 export class StandorteComponent implements OnInit {
-	mapLat = 48.5;
-	mapLng = 13.6880;
-	mapZoom = 0;
-	mapStyle: any;
-	locations = locationList;
 	urlPrefix = isDevMode() ? '../../' : './';
-	mapMarkerIcon = {
-		url: this.urlPrefix + 'assets/icons/loc.svg', 
-		labelOrigin: {x: 15, y: 30}, 
-		scaledSize: { width: 25, height: 25}
-	}
-	
 
-	constructor( private matDialog: MatDialog ) {
+	constructor( private matDialog: MatDialog) {
 		
 	}
 
 	ngOnInit(): void {
-		this.mapZoom = 5.5;
-		if (window.innerWidth < 500) {
-			this.mapLat = 50.3;
-			this.mapLng = 10;
-		}
-		
-		this.mapStyle = [
-			{
-				"elementType": "labels",
-				"stylers": [
-					{
-						"visibility": "off"
-					}
-				]
-			},
-			{
-				"featureType": "administrative",
-				"elementType": "geometry",
-				"stylers": [
-					{
-						"visibility": "off"
-					}
-				]
-			},
-			{
-				"featureType": "administrative.country",
-				"elementType": "geometry.fill",
-				"stylers": [
-					{
-						"color": "#007032"
-					},
-					{
-						"visibility": "on"
-					},
-					{
-						"weight": 8
-					}
-				]
-			},
-			{
-				"featureType": "administrative.country",
-				"elementType": "geometry.stroke",
-				"stylers": [
-					{
-						"color": "#007032"
-					},
-					{
-						"visibility": "on"
-					}
-				]
-			},
-			{
-				"featureType": "administrative.country",
-				"elementType": "labels.icon",
-				"stylers": [
-					{
-						"color": "#ff3def"
-					}
-				]
-			},
-			{
-				"featureType": "administrative.country",
-				"elementType": "labels.text.fill",
-				"stylers": [
-					{
-						"color": "#007032"
-					},
-					{
-						"visibility": "off"
-					},
-					{
-						"weight": 6.5
-					}
-				]
-			},
-			{
-				"featureType": "administrative.country",
-				"elementType": "labels.text.stroke",
-				"stylers": [
-					{
-						"visibility": "off"
-					}
-				]
-			},
-			{
-				"featureType": "administrative.land_parcel",
-				"stylers": [
-					{
-						"visibility": "off"
-					}
-				]
-			},
-			{
-				"featureType": "administrative.land_parcel",
-				"elementType": "geometry.fill",
-				"stylers": [
-					{
-						"visibility": "on"
-					}
-				]
-			},
-			{
-				"featureType": "administrative.neighborhood",
-				"stylers": [
-					{
-						"visibility": "off"
-					}
-				]
-			},
-			{
-				"featureType": "administrative.province",
-				"elementType": "geometry",
-				"stylers": [
-					{
-						"color": "#ff8b3d"
-					}
-				]
-			},
-			{
-				"featureType": "administrative.province",
-				"elementType": "geometry.fill",
-				"stylers": [
-					{
-						"color": "#ff8b3d"
-					},
-					{
-						"visibility": "on"
-					},
-					{
-						"weight": 2.5
-					}
-				]
-			},
-			{
-				"featureType": "administrative.province",
-				"elementType": "geometry.stroke",
-				"stylers": [
-					{
-						"color": "#ff8b3d"
-					}
-				]
-			},
-			{
-				"featureType": "poi",
-				"stylers": [
-					{
-						"visibility": "off"
-					}
-				]
-			},
-			{
-				"featureType": "road",
-				"stylers": [
-					{
-						"visibility": "off"
-					}
-				]
-			},
-			{
-				"featureType": "road",
-				"elementType": "labels.icon",
-				"stylers": [
-					{
-						"visibility": "off"
-					}
-				]
-			},
-			{
-				"featureType": "transit",
-				"stylers": [
-					{
-						"visibility": "off"
-					}
-				]
-			}
-		];
+
 	}
 	
 	showLocationDetails(location: any) {
