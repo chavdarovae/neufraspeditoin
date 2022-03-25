ed3b7c86e5aff43f5ac671a5b0cc6c5b49478181
Emiliya Chavdarova <emiliya@carrypicker.com>
2022-03-25 08:21:23 +0100

    Tryies to load the cities

--- src/app/standorte/standorte.component.ts
+++ src/app/standorte/standorte.component.ts
@@ -18,7 +18,7 @@ export class StandorteComponent implements OnInit {
 	) {}
 
 	ngOnInit(): void {
-		this.scriptService.loadJsScript(this.renderer, (this.urlPrefix + 'assets/image-map-pro/input.js'));
+		// this.scriptService.loadJsScript(this.renderer, (this.urlPrefix + 'assets/image-map-pro/input.js'));
 		this.scriptService.loadJsScript(this.renderer, (this.urlPrefix + 'assets/image-map-pro/map.js'));
 	}
 
