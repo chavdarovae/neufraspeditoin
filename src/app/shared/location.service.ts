1fe0452201a76ac69b8d68977e683ee1b184c54f
Emiliya Chavdarova <emiliya@carrypicker.com>
2022-03-29 12:58:06 +0200

    Fixes the mobile view of the location details view

--- src/app/shared/location.service.ts
+++ src/app/shared/location.service.ts
@@ -1,14 +1,15 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, isDevMode } from '@angular/core';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class LocationService {
+	urlPrefix = isDevMode() ? '../../' : './';
 
 	constructor(private http: HttpClient) { }
 
 	getLocationDetails(location: string) {
-		return this.http.get(`../../assets/files/locations/${location}.xlsx - ${location}.csv`, { responseType: 'text' });
+		return this.http.get(`${this.urlPrefix}assets/files/locations/${location}.xlsx - ${location}.csv`, { responseType: 'text' });
 	}
 }
